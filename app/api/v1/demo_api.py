495a983fb3108fee230d24d7e0f9aaff140f26a3
Neeraj Shukla <neerajshukla1911@gmail.com>
2018-06-24 19:18:38 +0530

    integrated swagger ui

--- app/api/v1/demo_api.py
+++ app/api/v1/demo_api.py
@@ -2,20 +2,28 @@ import logging
 from flask import jsonify
 from app.tasks import task_1
 
-
 logger = logging.getLogger(__name__)
 
 
-def demo_api():
+def get_demo_api():
     """
-    Skill
-    ---
-    parameters:
-     - name: data
-       in: body
-       required: true
-       type: json
+     GET Demo API
+     ---
+    responses:
+      200:
+        description: Returns GET demo api response
     """
     logger.debug("in demo api")
     task_1.delay()
     return jsonify({'message': "demo get api"}), 200
+
+
+def post_demo_api():
+    """
+     POST Demo API
+     ---
+    responses:
+      200:
+        description: Returns POST demo api response
+    """
+    return jsonify({'name': "demo post api"}), 200
