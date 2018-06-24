30ba16d8ef726442d2b481838a854c9225a959e7
Neeraj Shukla <neerajshukla1911@gmail.com>
2018-06-24 18:42:50 +0530

    list_routes cmd fix. changed class based view to function based view

--- app/api/v1/demo_api.py
+++ app/api/v1/demo_api.py
@@ -1,24 +1,21 @@
 import logging
-
 from flask import jsonify
-from flask.views import MethodView
-
 from app.tasks import task_1
+
 
 logger = logging.getLogger(__name__)
 
 
-class DemoApiView(MethodView):
+def demo_api():
     """
+    Skill
+    ---
+    parameters:
+     - name: data
+       in: body
+       required: true
+       type: json
     """
-    #@get_request.get(Model, GetApiSerializer, True, GetDemoApiHelper)
-    def get(self, *args, **kwargs):
-        logger.debug("in demo api")
-        task_1.delay()
-        return jsonify({'message': "demo get api"}), 200
-
-    # @post_request.post(CreateUpdateApiHelper, CreateUpdateDemoAPiSerializer, True)
-    def post(self):
-        """
-        """
-        return jsonify({'name': "demo post api"}), 200
+    logger.debug("in demo api")
+    task_1.delay()
+    return jsonify({'message': "demo get api"}), 200
--- app/api/v1/urls.py
+++ app/api/v1/urls.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
-from app.api.v1.demo_api import DemoApiView
+from app.api.v1.demo_api import demo_api
 
 api_v1 = Blueprint('api.v1', __name__)
 
-
-api_v1.add_url_rule('/demo-api/', view_func=DemoApiView.as_view('demo_api'))
+api_v1.add_url_rule('/demo-api/', view_func=demo_api)
